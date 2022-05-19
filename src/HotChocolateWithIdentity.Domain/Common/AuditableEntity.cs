24c6bb9f3dcbc9895c930a3f6c0f44f814265518
Alex Watts <alexjwatts83@gmail.com>
2022-05-20 07:47:47 +1000

    Added AuditableEntity

---
+++ src/HotChocolateWithIdentity.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotChocolateWithIdentity.Domain.Common
+{
+    public abstract class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedUtc { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedUtc { get; set; }
+    }
+}
