f29994cc495acdbed84f0c667886ff6f0f97b3cc
Alex Watts <alexjwatts83@gmail.com>
2022-05-21 08:52:31 +1000

    Added entities

---
+++ src/HotChocolateWithIdentity.Domain/Entities/TodoList.cs
@@ -0,0 +1,18 @@
+﻿using HotChocolateWithIdentity.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace HotChocolateWithIdentity.Domain.Entities
+{
+    public class TodoList : AuditableEntity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Colour { get; set; }
+
+        public IList<TodoItem> Items { get; set; }
+            = new List<TodoItem>();
+    }
+}
