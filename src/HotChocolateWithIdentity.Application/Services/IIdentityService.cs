e18dd7d0d8a8d0b8f0fcb68075423891381acb8d
Alex Watts <alexjwatts83@gmail.com>
2022-05-21 08:56:44 +1000

    Added IIdentityService

---
+++ src/HotChocolateWithIdentity.Application/Services/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace HotChocolateWithIdentity.Application.Services
+{
+    public interface IIdentityService
+    {
+        Task<string> GetUserNameAsync(string userId);
+
+        Task<(Result Result, string UserId)> CreateUserAsync(
+            string userName,
+            string password);
+
+        Task<Result> DeleteUserAsync(string userId);
+    }
+}
