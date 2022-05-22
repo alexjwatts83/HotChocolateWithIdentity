11c8bf2af7ecb347345f84bc5e83706a37f558cf
Alex Watts <alexjwatts83@gmail.com>
2022-05-22 14:36:17 +1000

    stuff

---
+++ src/HotChocolateWithIdentity.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HotChocolateWithIdentity.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(
+           this IServiceCollection services,
+           IConfiguration configuration)
+        {
+
+        }
+    }
+}
