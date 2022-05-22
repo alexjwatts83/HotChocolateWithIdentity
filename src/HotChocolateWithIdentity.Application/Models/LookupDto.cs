3e9d1d630594a735fa2e54e34a855792c50349f9
Alex Watts <alexjwatts83@gmail.com>
2022-05-22 14:09:11 +1000

    Added LookupDto

---
+++ src/HotChocolateWithIdentity.Application/Models/LookupDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotChocolateWithIdentity.Application.Models
+{
+    public class LookupDto
+    {
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+    }
+}
